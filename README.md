596bc98790770f17486791634f89cc53d5e197f8
余空 <70465933+YuKLiaa@users.noreply.github.com>
2021-02-13 12:21:01 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,21 @@
+Device configuration for Redmi K30 Ultra (cezanne)
+===================================================
+
+
+The Redmi K30 Ultra (codenamed _"cezanne"_) is a high-end, mid-range smartphone from Xiaomi.
+
+| Basic                   | Spec Sheet                                                                                                                     |
+| -----------------------:|:------------------------------------------------------------------------------------------------------------------------------ |
+| CPU                     | Octa-core                                                                                                                      |
+| Chipset                 | Mediatek Dimensity 1000+                                                                                                            |
+| GPU                     | Mali-G77 MC9                                                                                                                   |
+| Memory                  | 6/8 GB RAM                                                                                                                     |
+| Shipped Android Version | 10.0                                                                                                                            |
+| Storage                 | 128/256/512 GB                                                                                                                  |
+| Battery                 | Non-removable Li-Po 4500 mAh battery                                                                                           |
+| Display                 | 1080 x 2400 pixels                                                                            |
+
+
+
+**Copyright (C) 2020 The Potato Open Source Project**
+**Copyright (C) 2020 Magicial-Studio**
---
+++ lineage_cezanne.mk
@@ -0,0 +1,47 @@
+#
+# Copyright (C) 2019 Potato Open Sauce Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Dynamic
+PRODUCT_RELEASE_NAME := cezanne
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+DEVICE_PATH := device/redmi/cezanne
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root)
+# Inherit from begonia device
+$(call inherit-product, device/redmi/cezanne/device.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := cezanne
+PRODUCT_NAME := lineage_cezanne
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := Redmi K30 Ultra
+PRODUCT_MANUFACTURER := Xiaomi
+
+BUILD_FINGERPRINT := "Redmi/cezanne/cezanne:11/RP1A.200720.011/21.2.5:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="cezanne-user 11 RP1A.200720.011 21.2.5 release-keys" \
+    PRODUCT_NAME="cezanne" \
+    TARGET_DEVICE="cezanne"
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
